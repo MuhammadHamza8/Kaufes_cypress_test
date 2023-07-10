ed6de2b836bef0f179f4c098aa7de6bbc07f4699
MuhammadHamza8 <hamzaicp54@gmail.com>
2023-07-10 15:53:26 +0500

    files added

--- cypress/e2e/kaufes_signUp_test_script/signUp.UI.test.cy.js
+++ cypress/e2e/kaufes_signUp_test_script/signUp.UI.test.cy.js
@@ -1,85 +1,85 @@
-import { slowCypressDown } from "cypress-slow-down"
-slowCypressDown(500);
+// import { slowCypressDown } from "cypress-slow-down"
+// slowCypressDown(500);
 
-/// <reference types="cypress" />
-describe('SignUp Scenerio Testing', () => {
-    beforeEach(() => {
-      // Cypress starts out with a blank slate for each test
-      // so we must tell it to visit our website with the `cy.visit()` command.
-      // Since we want to visit the same URL at the start of all our tests,
-      // we include it in our beforeEach function so that it runs before each test
-      // cy.visit('https://staging.kaufes.ch/login');
+// /// <reference types="cypress" />
+// describe('SignUp Scenerio Testing', () => {
+//     beforeEach(() => {
+//       // Cypress starts out with a blank slate for each test
+//       // so we must tell it to visit our website with the `cy.visit()` command.
+//       // Since we want to visit the same URL at the start of all our tests,
+//       // we include it in our beforeEach function so that it runs before each test
+//       // cy.visit('https://staging.kaufes.ch/login');
     
-       cy.visit('https://staging.kaufes.ch/register');
+//        cy.visit('https://staging.kaufes.ch/register');
      
-    })
+//     })
 
-    it('responsiveness on iphon-3', () => {
+//     it('responsiveness on iphon-3', () => {
 
-        cy.viewport('iphone-3');
+//         cy.viewport('iphone-3');
 
-      })
+//       })
 
-      it('responsiveness on iphon-4', () => {
+//       it('responsiveness on iphon-4', () => {
 
-        cy.viewport('iphone-4');
+//         cy.viewport('iphone-4');
 
-      })
+//       })
 
-      it('responsiveness on iphon-5', () => {
+//       it('responsiveness on iphon-5', () => {
 
-        cy.viewport('iphone-5');
+//         cy.viewport('iphone-5');
 
-      })
+//       })
 
-      it('responsiveness on iphon-6', () => {
+//       it('responsiveness on iphon-6', () => {
 
-        cy.viewport('iphone-6');
+//         cy.viewport('iphone-6');
 
-      })
-
-
-      it('responsiveness on ipad-mini', () => {
-
-        cy.viewport('ipad-mini');
-
-      })
-
-      it('responsiveness on ipad-2', () => {
-
-        cy.viewport('ipad-2');
-
-      })
+//       })
 
 
-      it('responsiveness on macbook-11', () => {
+//       it('responsiveness on ipad-mini', () => {
 
-        cy.viewport('macbook-11');
+//         cy.viewport('ipad-mini');
 
-      })
+//       })
 
+//       it('responsiveness on ipad-2', () => {
 
-      it('responsiveness on macbook-15', () => {
+//         cy.viewport('ipad-2');
 
-        cy.viewport('macbook-15');
-
-      })
-
-      it('responsiveness on macbook-16', () => {
-
-        cy.viewport('macbook-16');
-
-      })
+//       })
 
 
-      it('responsiveness on mobile', () => {
+//       it('responsiveness on macbook-11', () => {
 
-        cy.viewport('iphone-6+');
+//         cy.viewport('macbook-11');
 
-      })
+//       })
 
 
+//       it('responsiveness on macbook-15', () => {
+
+//         cy.viewport('macbook-15');
+
+//       })
+
+//       it('responsiveness on macbook-16', () => {
+
+//         cy.viewport('macbook-16');
+
+//       })
+
+
+//       it('responsiveness on mobile', () => {
+
+//         cy.viewport('iphone-6+');
+
+//       })
 
 
 
-})
+
+
+// })
--- cypress/e2e/kaufes_signUp_test_script/signUp.content.test.cy.js
+++ cypress/e2e/kaufes_signUp_test_script/signUp.content.test.cy.js
@@ -1,40 +1,40 @@
-import { slowCypressDown } from "cypress-slow-down"
-slowCypressDown(500);
+// import { slowCypressDown } from "cypress-slow-down"
+// slowCypressDown(500);
 
-/// <reference types="cypress" />
-describe('SignUp Scenerio Testing', () => {
-    beforeEach(() => {
-      // Cypress starts out with a blank slate for each test
-      // so we must tell it to visit our website with the `cy.visit()` command.
-      // Since we want to visit the same URL at the start of all our tests,
-      // we include it in our beforeEach function so that it runs before each test
-      // cy.visit('https://staging.kaufes.ch/login');
+// /// <reference types="cypress" />
+// describe('SignUp Scenerio Testing', () => {
+//     beforeEach(() => {
+//       // Cypress starts out with a blank slate for each test
+//       // so we must tell it to visit our website with the `cy.visit()` command.
+//       // Since we want to visit the same URL at the start of all our tests,
+//       // we include it in our beforeEach function so that it runs before each test
+//       // cy.visit('https://staging.kaufes.ch/login');
     
-       cy.visit('https://staging.kaufes.ch/register');
+//        cy.visit('https://staging.kaufes.ch/register');
      
-    })
+//     })
 
 
-    it('verifying Create Account text ', () => {
+//     it('verifying Create Account text ', () => {
 
-       cy.contains('Create Account').should('exist');
+//        cy.contains('Create Account').should('exist');
 
-      })
+//       })
 
-      it('verifying  Join free! Please enter your details text ', () => {
+//       it('verifying  Join free! Please enter your details text ', () => {
 
-        cy.contains('Join free! Please enter your details').should('exist');
+//         cy.contains('Join free! Please enter your details').should('exist');
  
-       })
+//        })
  
-       it('verifying  Already have an account ? text ', () => {
+//        it('verifying  Already have an account ? text ', () => {
 
-        cy.contains('Already have an account ? ').should('exist');
+//         cy.contains('Already have an account ? ').should('exist');
  
-       })
+//        })
 
 
 
 
 
-})
+// })
